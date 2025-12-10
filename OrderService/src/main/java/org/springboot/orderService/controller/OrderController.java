8dcc9d5fb158ff809c7a35115e4978fb2e8d4311
Aniket.Singh <Aniket.Singh@PBLAP103.probuddy.corp>
2025-12-10 11:06:37 +0530

    Producing order creation notification

--- OrderService/src/main/java/org/springboot/orderService/controller/OrderController.java
+++ OrderService/src/main/java/org/springboot/orderService/controller/OrderController.java
@@ -1,6 +1,7 @@
 package org.springboot.orderService.controller;
 
 
+import org.springboot.orderService.dto.OrderNotification;
 import org.springboot.orderService.model.Order;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,5 +30,14 @@ public class OrderController
     {
         rabbitTemplate.convertAndSend(exchange, routingKey, order);
         return ResponseEntity.ok("✅ Order sent to RabbitMQ: " + order.getOrderId());
+    }
+    
+    @PostMapping("/notifications")
+    public OrderNotification sendNotification()
+    {
+    	OrderNotification notification = new OrderNotification();
+    	rabbitTemplate.convertAndSend(exchange, routingKey, notification);
+    	return notification;
+    	
     }
 }
